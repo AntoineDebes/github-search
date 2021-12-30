6f4c5c1e7c8968e9c736d895f2eda970046d0d99
Antoinedebes <antoinedebes@gmail.com>
2021-12-30 22:53:57 +0200

    fix: Added some dependencies

--- Nodejs/src/server.ts
+++ Nodejs/src/server.ts
@@ -1,6 +1,6 @@
 import app from "./index";
 
-const PORT = process.env.PORT || 8082;
+const PORT = process.env.PORT || 9082;
 
 app.listen(PORT, () => {
   console.log(`The server is listening on port ${PORT}`);
